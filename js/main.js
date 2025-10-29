52f59e722eba8ddabdba1f1218f355f8250f8296
Nemi1230 <nemiakuma@gmail.com>
2025-10-29 22:22:57 +0900

    add dropdown list

--- js/main.js
+++ js/main.js
@@ -10,4 +10,23 @@ $(function () {
         $(this).addClass('--active');
         $(`.tab-content[data-tab="${target}"]`).addClass('--show');
     });
+
+    $('.select-box .selected').on('click', function (e) {
+        const parent = $(this).parent();
+        parent.toggleClass('--open');
+        e.stopPropagation();
+    });
+
+    $('.select-box .options li').on('click', function () {
+        const parent = $(this).closest('select-box');
+        parent.find('.selected').text($(this).text());
+        parent.removeClass('--open');
+
+        const type = $('#sortType .selected').text();
+        const order = $('#sortOrder .selected').text();
+    });
+
+    $(document).on('click', function () {
+        $('.select-box').removeClass('--open');
+    });
 });
